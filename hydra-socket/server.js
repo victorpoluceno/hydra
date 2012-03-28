59ab0336af9bbb967f31b8758e450259a2d58ef1
Victor Godoy Poluceno <victorpoluceno@gmail.com>
2012-03-28 14:14:50 -0300

    Cleanup

--- hydra-socket/server.js
+++ hydra-socket/server.js
@@ -1,12 +1,12 @@
 var io = require('socket.io').listen(8080);
 var sqlite3 = require('sqlite3').verbose();
 
-var db = new sqlite3.Database('hydra/hydra.db');
+var db = new sqlite3.Database('../hydra-www/hydra.db');
 
 io.enable('browser client minification'); // send minified client
 io.enable('browser client etag'); // apply etag caching logic based on version number
 io.enable('browser client gzip'); // gzip the file
-//io.set('log level', 1); // reduce logging
+io.set('log level', 1); // reduce logging
 io.set('transports', [ // enable all transports (optional if you want flashsocket)
     'websocket'
   , 'flashsocket'
@@ -17,30 +17,28 @@ io.set('transports', [ // enable all transports (optional if you want flashsocke
 
 io.sockets.on('connection', function (socket) {
     socket.on('load', function (guid, fn) {
-	var list = new Array();
+    	var list = new Array();
 
-	db.each("SELECT m.original_file, pl.sort FROM player_movie m, " +
-	      "player_playlist p, " +
-	      "player_playlistmovie pl, " +
-	      "player_device d " +
-	      "WHERE m.id=pl.movie_id and pl.playlist_id=p.id and " +
-	      "d.playlist_id=p.id and d.guid=? ", guid,
-	    function(err, row) {
-	    if (err != null){
-	        console.log(err);  
-	        console.log(row);  
-	    }
-	    if (row){
-	        console.log(row);
-                list.push("media/player/" + row.original_file);
+    	db.each("SELECT m.original_file, pl.sort FROM player_movie m, " +
+    	      "player_playlist p, " +
+    	      "player_playlistmovie pl, " +
+    	      "player_device d " +
+    	      "WHERE m.id=pl.movie_id and pl.playlist_id=p.id and " +
+    	      "d.playlist_id=p.id and d.guid=? ", guid,
+    	    function(err, row) {
+                if (err != null){
+                    console.log(err);  
+                    console.log(row);  
+                }
+    	    
+                if (row){
+                    console.log(row);
+                    list.push("media/player/" + row.original_file);
+                }
+            }, function (){
+                console.log(list);
+                fn(list);
             }
-	}, function (){
-       		console.log(list);
-			fn(list);
-		}
-	);
+        );
     });
-    //socket.emit('set', {list: videoSourceList});
 });
-
-//db.close();

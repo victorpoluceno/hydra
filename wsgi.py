ec064b7bf17aa6558daa146ac9d05edaba598b73
Victor Godoy Poluceno <victorpoluceno@gmail.com>
2012-03-28 00:25:39 -0300

    Add player path

--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,7 @@
 import os
+import sys
 os.environ['DJANGO_SETTINGS_MODULE'] = 'hydra.settings_production'
+sys.path.insert(0, "player/")
+
 import django.core.handlers.wsgi
 application = django.core.handlers.wsgi.WSGIHandler()

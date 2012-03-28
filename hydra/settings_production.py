52f30a55475a7c2cc281750b8cdb75ff7efd2baa
Victor Godoy Poluceno <victorpoluceno@gmail.com>
2012-03-27 22:58:54 -0300

    Dotcloud support

--- hydra/settings_production.py
+++ hydra/settings_production.py
@@ -43,18 +43,18 @@ USE_I18N = True
 USE_L10N = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
-MEDIA_ROOT = '/srv/hydra/media/player/'
+MEDIA_ROOT = '/home/dotcloud/data/media/'
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
-MEDIA_URL = '/media/player/'
+MEDIA_URL = '/media/'
 
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = '/srv/hydra/static/'
+STATIC_ROOT = '/home/dotcloud/volatile/static/'
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
--- postinstall.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-sudo mkdir -p /srv/hydra/media /srv/hydra/static
-cd hydra; sudo python manage.py collectstatic --noinput --settings=settings_production

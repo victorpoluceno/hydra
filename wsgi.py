8030a820c998c11f0d82591dd76dfad9b8d6ea80
Victor Godoy Poluceno <victorpoluceno@gmail.com>
2012-03-28 00:10:45 -0300

    Use right settings

--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,4 @@
 import os
-os.environ['DJANGO_SETTINGS_MODULE'] = 'hydra.settings'
+os.environ['DJANGO_SETTINGS_MODULE'] = 'hydra.settings_production'
 import django.core.handlers.wsgi
 application = django.core.handlers.wsgi.WSGIHandler()

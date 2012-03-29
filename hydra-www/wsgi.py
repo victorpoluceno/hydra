25ce3d7a8b796694aa14dc4d881c626fa15df36f
Victor Godoy Poluceno <victorpoluceno@gmail.com>
2012-03-29 17:31:12 -0300

    Support for django and socketio

--- hydra-www/wsgi.py
+++ hydra-www/wsgi.py
@@ -1,9 +1,14 @@
+from gevent import monkey; monkey.patch_all()
+
 import os
 import sys
+
 os.environ['DJANGO_SETTINGS_MODULE'] = 'hydra.settings_production'
 PROJECT_ROOT = os.path.dirname(__file__)
 sys.path.insert(0, os.path.join(PROJECT_ROOT, "hydra/"))
 
 import django.core.handlers.wsgi
+from socketio.handler import WebSocketHandler
+
 application_django = django.core.handlers.wsgi.WSGIHandler()
 application = WebSocketHandler('0.0.0.0', 80, application_django, 'socket.io')

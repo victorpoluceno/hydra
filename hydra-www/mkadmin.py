8ad791da158686091a060c8ff1f1df3813815ef9
Victor Godoy Poluceno <victorpoluceno@gmail.com>
2012-03-29 22:10:37 -0300

    Set DJANGO_SETTINGS

--- hydra-www/mkadmin.py
+++ hydra-www/mkadmin.py
@@ -1,5 +1,11 @@
 #!/usr/bin/env python
-from wsgi import *
+import os
+import sys
+os.environ['DJANGO_SETTINGS_MODULE'] = 'hydra.settings_production'
+PROJECT_ROOT = os.path.dirname(__file__)
+sys.path.insert(0, os.path.join(PROJECT_ROOT, "hydra/"))
+
+
 from django.contrib.auth.models import User
 u, created = User.objects.get_or_create(username='admin')
 if created:

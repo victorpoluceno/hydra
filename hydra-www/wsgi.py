b4ad486edcb8d3c9ed1e15aded695f18acd489b3
Victor Godoy Poluceno <victorpoluceno@gmail.com>
2012-03-31 12:47:04 -0300

    Fix to use settings_production

--- hydra-www/wsgi.py
+++ hydra-www/wsgi.py
@@ -5,14 +5,14 @@ monkey.patch_all()
 
 import os
 
-from hydra import settings as settings
+from hydra import settings_production
 
 import django.core.handlers.wsgi
 from socketio import SocketIOServer
 
 from django.core.management import setup_environ
-setup_environ(settings)
+setup_environ(settings_production)
 
 
-os.environ['DJANGO_SETTINGS_MODULE'] = 'hydra.settings'
+os.environ['DJANGO_SETTINGS_MODULE'] = 'hydra.settings_production'
 application = django.core.handlers.wsgi.WSGIHandler()

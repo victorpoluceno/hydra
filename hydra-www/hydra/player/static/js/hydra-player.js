aca5f96425417b80c23df418ab8f21430e49077a
Victor Godoy Poluceno <victorpoluceno@gmail.com>
2012-04-02 15:45:33 -0300

    Disable load

--- hydra-www/hydra/player/static/js/hydra-player.js
+++ hydra-www/hydra/player/static/js/hydra-player.js
@@ -7,6 +7,6 @@ function play(path){
     var name = path.substr(0, path.lastIndexOf('.')) || path; 
     $("video")[0].src = name + "." + extension;
     $("video")[0].type = "video/" + extension;
-    $("video")[0].load();
+    //$("video")[0].load();
     $("video")[0].play();    
 }

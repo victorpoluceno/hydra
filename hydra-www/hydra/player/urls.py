3c1758fac12ffe3c56d0033e3ec5172b353f420f
Victor Godoy Poluceno <victorpoluceno@gmail.com>
2012-04-01 22:58:15 -0300

    Fix wrong view function name

--- hydra-www/hydra/player/urls.py
+++ hydra-www/hydra/player/urls.py
@@ -5,7 +5,7 @@ from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 urlpatterns = patterns('',
     url(r'^$', 'player.views.index'),
     url(r'^device$', 'player.views.device'),
-    url(r'^socket.io', 'player.views.api'),
+    url(r'^socket.io', 'player.views.socketio'),
 )
 
 urlpatterns += staticfiles_urlpatterns()

55ea4fff5ac556f7d13b0064586c4f3daaede5a2
Victor Godoy Poluceno <victorpoluceno@gmail.com>
2012-04-02 23:47:20 -0300

    Bla

--- hydra-www/hydra/player/static/js/hydra-player.js
+++ hydra-www/hydra/player/static/js/hydra-player.js
@@ -6,13 +6,17 @@ function play(path){
     // remove extension
     var name = path.substr(0, path.lastIndexOf('.')) || path;
     
-    $('video').remove();
-
-    var t = $('<video id="player" class="player" width="100%" height="100%" loop></video>');
+    var t = $('<video id="player" class="player" width="100%" height="100%" loop>
+            <source>
+        </video>');
     $("body").append(t);
 
-    $("video")[0].type = "video/" + extension;
-    $("video")[0].src = name + "." + extension;
+    //getElementByTagName('video')[0].addEventListener('error', function(event) { 
+    //    console.log(event); 
+    //}, true);
+
+    $("source")[0].type = "video/" + extension;
+    $("source")[0].src = name + "." + extension;
     $("video")[0].load();
-    $("video")[0].play();    
+    $("video")[0].play();
 }

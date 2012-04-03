b27081911d891059a769430b97a36d211f4199cf
Victor Godoy Poluceno <victorpoluceno@gmail.com>
2012-04-02 22:34:39 -0300

    Recreate element every time

--- hydra-www/hydra/player/static/js/hydra-player.js
+++ hydra-www/hydra/player/static/js/hydra-player.js
@@ -5,12 +5,14 @@ function play(path){
 
     // remove extension
     var name = path.substr(0, path.lastIndexOf('.')) || path;
+    
+    $('video').remove();
+
+    var t = $('<video id="player" class="player" width="100%" height="100%" loop></video>');
+    $("body").append(t);
+
     $("video")[0].type = "video/" + extension;
     $("video")[0].src = name + "." + extension;
     $("video")[0].load();
-    //$("video")[0].play();    
+    $("video")[0].play();    
 }
-
-$("video")[0].addEventListener('canplay', function(e) {
-  this.play();
-}, false);
